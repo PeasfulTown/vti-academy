b32c13509570a35f3ef8f26413d2d018179eab14
PeasfulTown <peasfultown@gmail.com>
2026-02-27 19:25:14 +0700

    chore: added eureka server and client discovery

---
+++ 4.microservices/2.hrm_microservices/eureka_server/src/test/java/xyz/peasfultown/hrm/eureka_server/EurekaServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.peasfultown.hrm.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
